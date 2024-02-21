69b5dd4cf0010efac85dca0892c428e16dfe8adf
shota876 <sh.guliashvili1@gmail.com>
2024-02-21 07:21:37 -0800

    #4 connected to the database.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose, { mongo } from "mongoose"
 
 const app = express()
+dotenv.config()
+const MONGO_URL = process.env.MONGO_URL
 
 app.use(cors({
     credentials: true,
@@ -19,3 +23,12 @@ const server = http.createServer(app)
 server.listen(8080, () => {
     console.log('server running on localhost')
 })
+
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL).then(() => console.log('connected'))
+// error catcher
+mongoose.connection.on('connecnted', () => console.log('hello mongo'))
+
+
+
