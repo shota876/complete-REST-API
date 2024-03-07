88bac05d76e38e37cd924846369d3ab93cae345d
shota876 <sh.guliashvili1@gmail.com>
2024-03-07 09:08:11 -0800

    #13 added update route.

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -23,3 +23,22 @@ export const deleteUser = async (req: Express.Request, res: Express.Response) =>
         return res.sendStatus(400)
     }
 }
+
+export const updateUser = async (req: Express.Request, res: Express.Response) => {
+    try{
+        const { id } = req.params
+        const { username } = req.body
+    
+        if(!username) return res.sendStatus(400)
+    
+        const user = await getUserById(id)
+    
+        user.username = username
+        await user.save()
+    
+        return res.status(200).json(user).end()
+    }catch(error){
+        console.log(error)
+        return res.sendStatus(400)
+    }
+}
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import Express from 'express';
-import { deleteUser, getAllUsers } from '../controllers/users.ts';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users.ts';
 import { isAuthenticated, isOwner } from '../middlewares/index.ts';
 
 
 export default (router: Express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers)
     router.delete('/users/:id', isAuthenticated, isOwner, deleteUser)
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser)
 }
